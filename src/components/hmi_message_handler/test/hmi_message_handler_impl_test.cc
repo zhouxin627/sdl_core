c0da526bfbc8223c0b49962e9ef674ed8bcaf6d5
ypostolov <ypostolov@luxoft.com>
2020-01-09 20:03:57 +0200

    fix cppcheck issues in component hmi_message_handler

--- src/components/hmi_message_handler/test/hmi_message_handler_impl_test.cc
+++ src/components/hmi_message_handler/test/hmi_message_handler_impl_test.cc
@@ -92,6 +92,7 @@ class HMIMessageHandlerImplTest : public ::testing::Test {
   }
 };
 
+// cppcheck-suppress syntaxError
 TEST_F(HMIMessageHandlerImplTest,
        OnErrorSending_EmptyMessage_OnErrorSendingProceeded) {
   // Arrange
